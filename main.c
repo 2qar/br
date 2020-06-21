6577b0b235890b095c1277d56ab70b4f9b41b6d4
tuckerrrrrrrrrr <tucker1014@hotmail.com>
2020-06-20 19:16:40 -0700

    use strncmp instead of the crappy streq function i wrote

--- main.c
+++ main.c
@@ -5,8 +5,6 @@
 #define TMP_PATH "/tmp/br"
 #define MAX_FILENAME_LEN 1000
 
-int streq(char *, char *);
-
 int main(int argc, char **argv) {
 	if (argc == 1) {
 		fprintf(stderr, "usage: br [FILE]...\n");
@@ -45,7 +43,7 @@ int main(int argc, char **argv) {
 	size_t max_len = MAX_FILENAME_LEN;
 	while ((n = getline(&name, &max_len, f)) > 0) {
 		name[n-1] = 0;
-		if (!streq(argv[i], name)) {
+		if (strncmp(argv[i], name, MAX_FILENAME_LEN) != 0) {
 			printf("%s -> %s\n", argv[i], name);
 			if (rename(argv[i], name) < 0) {
 				perror("rename");
@@ -56,19 +54,4 @@ int main(int argc, char **argv) {
 	free(name);
 
 	remove(TMP_PATH);
-}
-
-int streq(char *s1, char *s2) {
-	int s1len = strlen(s1);
-	int s2len = strlen(s2);
-
-	if (s1len != s2len) {
-		return 0;
-	}
-
-	for (int i = 0; i < s1len; ++i)
-		if (s1[i] != s2[i])
-			return 0;
-
-	return 1;
 }
