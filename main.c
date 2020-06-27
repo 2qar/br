167816542a810f978b0805891e2130cc67470c4c
tucker <tucker1014@hotmail.com>
2020-06-27 09:54:48 -0700

    add prompt flag -p

--- main.c
+++ main.c
@@ -11,10 +11,17 @@ int main(int argc, char **argv) {
 		exit(1);
 	}
 
-	FILE *f = fopen(TMP_PATH, "w");
-	for (int i = 1; i < argc; ++i) {
-		fprintf(f, "%s\n", argv[i]);
+	int prompt = 0;
+
+	int i = 1;
+	for (; argv[i][0] == '-'; ++i) {
+		if (argv[i][1] == 'p')
+			prompt = 1;
 	}
+
+	FILE *f = fopen(TMP_PATH, "w");
+	for (int fi = i; fi < argc; ++fi)
+		fprintf(f, "%s\n", argv[fi]);
 	fclose(f);
 
 	char *editor;
@@ -30,7 +37,7 @@ int main(int argc, char **argv) {
 	char tmp[MAX_FILENAME_LEN];
 	while (fgets(tmp, MAX_FILENAME_LEN, f))
 		++n;
-	if (n != argc -1) {
+	if (n != (argc - i)) {
 		fprintf(stderr, "br: you added or deleted lines. dont do that\n");
 		fclose(f);
 		exit(1);
@@ -38,18 +45,28 @@ int main(int argc, char **argv) {
 
 	rewind(f);
 
-	int i = 1;
 	char *name = malloc(sizeof(char) * MAX_FILENAME_LEN);
 	size_t max_len = MAX_FILENAME_LEN;
 	while ((n = getline(&name, &max_len, f)) > 0) {
 		name[n-1] = 0;
 		if (strncmp(argv[i], name, MAX_FILENAME_LEN) != 0) {
-			printf("%s -> %s\n", argv[i], name);
+			printf("%s -> %s", argv[i], name);
+			if (prompt) {
+				printf("? [y/N]: ");
+				char answer[10] = {0};
+				fgets(answer, 10, stdin);
+				if (answer[0] != 'y') {
+					++i;
+					continue;
+				}
+			}
 			if (rename(argv[i], name) < 0) {
 				perror("rename");
 			}
+			if (!prompt)
+				putchar('\n');
 		}
-		i += 1;
+		++i;
 	}
 	free(name);
 
